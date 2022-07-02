cfb318e6d4cab1d81937ee21d20024006bc08892
Joan Wilson <joanwmoliveira@gmail.com>
2022-07-02 01:49:12 -0300

    first commit

---
+++ comfy-food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  comfy-food
+//
+//  Created by Joan Wilson Oliveira on 02/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
